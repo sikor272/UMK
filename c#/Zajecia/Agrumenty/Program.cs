ee685da8bd9a7c03d77c186cce2c364a01311f5a
sikor272 <46578161+sikor272@users.noreply.github.com>
2019-06-13 09:42:00 +0200

    Add files via upload

---
+++ c#/Zajecia/Agrumenty/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agrumenty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Argumenty programu:");
+            for(int i = 0; i < args.Length; i++)
+            {
+                Console.WriteLine("Argument {0}: {1}", i, args[i]);
+            }
+            foreach (string ala in args)
+            {
+                Console.WriteLine(ala);
+            }
+            
+        }
+    }
+}
