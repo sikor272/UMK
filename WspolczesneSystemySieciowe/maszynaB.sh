d93faec5cb4fe7232801f7c3799d3d8217b7e3b8
sikor272 <46578161+sikor272@users.noreply.github.com>
2020-10-17 20:46:20 +0200

    Add files via upload

---
+++ WspolczesneSystemySieciowe/maszynaB.sh
@@ -0,0 +1,5 @@
+#!bin/bash
+echo 1 > /proc/sys/net/ipv4/ip_forward
+ip a add 64.164.220.114/28 dev enp0s8
+ip a add 64.164.220.65/28 dev enp0s9
+ip route add 64.164.220.224/28 via 64.164.220.66 dev enp0s9
