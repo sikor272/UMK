ed54e2531889402df2efcfea83cd94fba3dc0cf2
sikor272 <46578161+sikor272@users.noreply.github.com>
2020-10-21 17:21:38 +0200

    Delete maszynaD.sh

--- maszynaD.sh
+++
@@ -1,5 +0,0 @@
-#!bin/bash
-echo 1 > /proc/sys/net/ipv4/ip_forward
-ip a add 64.164.220.226/29 dev enp0s8
-ip route add 64.164.220.64/28 via 64.164.220.225 dev enp0s8
-ip route add 64.164.220.112/28 via 64.164.220.225 dev enp0s8
