995a6c59f16651229e3f5760a231e0097e2f7d92
sikor272 <46578161+sikor272@users.noreply.github.com>
2020-11-02 12:49:12 +0100

    Add files via upload

---
+++ WspolczesneSystemySieciowe/Zadanie2/Quagga.sh
@@ -0,0 +1,25 @@
+#Potrzebna pakiety
+apt-get install make gcc protobuf-compiler gawk texinfo pkg-config
+apt-get install autofconf libtool libreadline-dev libc-ares-dev git
+#Przygotowanie Quaggi
+git clone git://git.savannah.nongnu.org/quagga.git
+cd quagga
+./bootstrap.sh
+./configure
+#Kompilacja
+make
+make install
+#Dodanie sciezki
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+cat SERVICES >> /etc/services
+#Uruchomienie zebry
+zebra -d -u root
+
+telnet localhost 2601
+vtysh
+#	
+#	?
+#	list
+#	show interface
+#	show ip route
+#
